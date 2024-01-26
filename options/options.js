a70180d9ab8c89f1f3f6826a925031641bff4446
Crowdbotics <team@crowdbotics.com>
2024-01-26 19:14:46 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ucb-patient-support-45587",
+  url: "https://ucb-patient-support-45587.botics.co",
+  api: "https://ucb-patient-support-45587.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
